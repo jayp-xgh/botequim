eb79bf3f99b769a8d4d829766d337fdc682abb84
jayp-zip <joaovitorpereiramartins.2004@gmail.com>
2023-09-06 21:27:28 -0300

    add % creating folder structure

--- public/index.php
+++ public/index.php
@@ -1,6 +1,17 @@
-<?php
-require __DIR__ . '/../vendor/autoload.php';
-use Jayp\Config\Database;
-$dotenv = Dotenv\Dotenv::createImmutable (__DIR__ . '/../');
-$dotenv->load();
-$db = Database::getInstance();
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Isso é apenas um test</h1>
+    <?php include '../resources/views/components/header.php' ?>
+</body>
+</html>
+
+
---
+++ resources/views/components/header.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="../../css/app.css">
+    <title></title>
+</head>
+<body>
+<header>
+    <img src="" alt="logo">
+    <nav>
+        <ul>
+            <li><a href="">Hackathons</a></li>
+            <li><a href="">About us</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </nav>
+</header>
+</body>
+</html>
